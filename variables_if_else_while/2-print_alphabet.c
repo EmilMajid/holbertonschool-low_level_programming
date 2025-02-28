e0e50627536539fe284de5b1f3ae09bc9902110a
EmilMajid <emilinpoctu@gmail.com>
2025-02-28 09:09:51 +0000

    2-print_alphabet.c-2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,10 @@
  */
 int main(void)
 {
-	int i;
-	for (int i = 97; i <= 122; i++)
-	{
-		putchar(i);
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	
+		putchar(letter);
 		putchar('\n');
-	}
 	return (0);
 }
