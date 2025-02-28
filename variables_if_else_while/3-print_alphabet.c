615c9efdeab6157413abc17d718299a65ac22df0
EmilMajid <emilinpoctu@gmail.com>
2025-02-28 10:41:54 +0000

    3-print_alphabet

---
+++ variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * program prints the alphabet in lowercase and  uppercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+	char A;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
