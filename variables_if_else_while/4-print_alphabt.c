710adf8887c3ee65145cf0ed6d419c4de0d1bb5c
EmilMajid <emilinpoctu@gmail.com>
2025-03-01 16:19:57 +0000

    4-print_alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * program prints the alphabet, print all the letters except q and e
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'q' && a != 'e') 
+
+		{
+			putchar(a);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
