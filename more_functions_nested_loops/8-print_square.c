37d1ab50cc3f14f3a12f547769a73769161f7976
EmilMajid <emilinpoctu@gmail.com>
2025-03-05 10:51:00 +0000

    8-squares-3

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,27 @@
 #include "main.h"
-
 /**
  * print_square - Prints a square using the character #.
+ *
  * @size: The size of the square.
  *
  * If size is 0 or less, the function prints only a new line.
  */
 void print_square(int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
