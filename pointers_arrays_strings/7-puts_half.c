c853278edf6bddfb272ed3d145db33dd9cc1697c
EmilMajid <emilinpoctu@gmail.com>
2025-03-10 12:28:00 +0000

    7-puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+*puts_half - Writes the second half of the string.
+*@str: the string that should be written.
+*
+*Return: void.
+*/
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		i = length / 2;
+	}
+	else
+	{
+		i = (length + 1) / 2;
+	}
+	while (str[i] != 0)
+	{
+		i++;
+	}
+	_putchar('\n');
+}
