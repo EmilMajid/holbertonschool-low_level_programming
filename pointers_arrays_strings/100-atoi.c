23a53e8d0bfaba7fb57b0defbf5819e1f07985d6
EmilMajid <emilinpoctu@gmail.com>
2025-03-11 07:46:59 +0000

    100-atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*_atoi - Converts the string to an integer.
+*@s: the string that should be converted
+*
+*Return: Integer after convertion.
+*/
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int result = 0;
+
+	while (*s)
+	{
+		if (*s == 45)
+			sign *= -1;
+		if (*s >= 48 && *s <= 57)
+		{
+			result = result * 10 + (*s - 48);
+			if (*(s + 1) < 48 || *(s + 1) > 57)
+			{
+				break;
+			}
+		}
+		s++;
+	}
+	return (result * sign);
+}
