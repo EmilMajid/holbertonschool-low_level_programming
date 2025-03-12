ce2df40f34ad9d0d6f503db6f2a86b7f1095982a
EmilMajid <emilinpoctu@gmail.com>
2025-03-12 08:10:40 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' && s2[2] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[1] != s2[2])
+		if (s1[i] != s2[i])
 		{
-			return (s1[1] - s2[2]);
+			return (s1[i] - s2[i]);
 		}
+
 		i++;
 	}
-	return (s1[1] - s2[2]);
+
+	return (s1[i] - s2[i]);
 }
