e90ab8b1afa5c181ba0fb8d83cb35802385ca0d9
EmilMajid <emilinpoctu@gmail.com>
2025-03-22 14:01:48 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,25 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef MAIN_H_
+#define MAIN_H_
+
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+*struct dog - Defines a dog.
+*@name: Name of the dog
+*@age: Age of the dog
+*@owner: Owner of the dog
+*
+*Description: Defines a dog with its name, age and owner.
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
