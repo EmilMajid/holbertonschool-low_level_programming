88845f5c2d26791c739fe3c59a0532a24e66ace6
EmilMajid <emilinpoctu@gmail.com>
2025-04-08 14:15:22 +0000

    1-list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+*print_list - Writes the given list.
+*@h: Given list
+*
+*Return: The number of nodes.
+*/
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
