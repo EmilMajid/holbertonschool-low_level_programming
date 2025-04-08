853e03f93c9e13edb3735f26d31817d156c92a37
EmilMajid <emilinpoctu@gmail.com>
2025-04-08 14:21:10 +0000

    betty 1-list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
-*print_list - Writes the given list.
+*list_len - Calculates number of elements in the given list.
 *@h: Given list
-*
 *Return: The number of nodes.
 */
+
 size_t list_len(const list_t *h)
 {
 	int i = 0;
