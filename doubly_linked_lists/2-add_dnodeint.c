102489b2d45a511a2762a8160227ae58fe8ae00f
EmilMajid <emilinpoctu@gmail.com>
2025-04-16 17:19:18 +0000

    task 2-1

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,43 +1,28 @@
 #include "lists.h"
-/**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: double pointer to list.
- * @idx: index of inserting position.
- * @n: data for new node insertion.
- * Return: the address of the new node.
- */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{
-	unsigned int i = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *tmp_node = *h;
+#include <stdlib.h>
 
-	if (new_node)
-	{
-		new_node->n = n;
-		if (idx == 0)
-		{
-			if (*h)
-			{
-				new_node->next = *h;
-				(*h)->prev = new_node;
-			}
-			*h = new_node;
-			return (new_node);
-		}
-		while (i < idx - 1 && tmp_node)
-		{
-			tmp_node = tmp_node->next;
-			i++;
-		}
-		if (tmp_node)
-		{
-			if (tmp_node->next)
-				tmp_node->next->prev = new_node;
-			new_node->next = tmp_node->next;
-			new_node->prev = tmp_node;
-			tmp_node->next = new_node;
-			return (new_node);
-		}
-	}
-	return (NULL);
+/**
+ * add_dnodeint - adds a new node at the beginning of a `dlistint_t` list.
+ * @head: pointer to the head pointer of the list
+ * @n: data for the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+
+	if (*head)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+
+	return (new_node);
 }
